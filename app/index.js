73dbb7bcd9c148844965e8743dee902a0c8d2242
SuperiorSU <sujalsinha2001@gmail.com>
2024-03-05 11:27:48 +0530

    Project Initialised

---
+++ app/index.js
@@ -0,0 +1,14 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const index = () => {
+  return (
+    <View>
+      <Text className="text-[32px] font-bold p-5 bg-purple-700 text-white">
+        Hello User. This is test text.
+      </Text>
+    </View>
+  )
+}
+
+export default index
